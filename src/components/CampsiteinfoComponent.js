59743b4f272f449554bda11dbf877ffdb7dc2e95
Ryan Pool <rlpool06@gmail.com>
2020-12-29 20:22:27 -0600

    12-29

--- src/components/CampsiteinfoComponent.js
+++ src/components/CampsiteinfoComponent.js
@@ -1,13 +1,8 @@
-import { Component } from "react";
+import React from "react";
 import {Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
-class CampsiteInfo extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    renderCampsite(campsite) {
+function RenderCampsite({campsite}) {
         return (
             <div className="col-md-5 m-1">
                 <Card>
@@ -21,7 +16,7 @@ class CampsiteInfo extends Component {
         );
     }
         
-    renderComments(comments) {
+function RenderComments({comments}) {
         if (comments) {
             return (
                 <div className="col-md-5 m-1">
@@ -39,22 +34,22 @@ class CampsiteInfo extends Component {
         }
     }
 
-    render() {
-        if (this.props.campsite) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        {this.renderCampsite(this.props.campsite)}
-                        {this.renderComments(this.props.campsite.comments)}
-                    </div>
+function CampsiteInfo(props) {
+    if (props.campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.campsite.comments} />
                 </div>
-            )
-        } else {
-            return (
-                <div />
-            )
-        }
+            </div>
+        )
+    } else {
+        return (
+            <div />
+        )
     }
 }
+
 
 export default CampsiteInfo;
